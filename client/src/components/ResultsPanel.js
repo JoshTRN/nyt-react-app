681e188a116308b20f167d9a690a666de2fb613d
Josh <josh@Joshs-MacBook-Pro.local>
2018-06-23 01:20:02 -0400

    added client folder

---
+++ client/src/components/ResultsPanel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ResultPanel = () => (
+
+    <div class="card container mb-5 col-lg-8">
+        <div class="card-header text-center bg-secondary text-white">
+            Results
+        </div>
+
+        <div class="card-body">
+        </div>
+    </div>
+
+)
+
+export default ResultPanel
